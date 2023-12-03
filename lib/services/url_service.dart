21cfc7a36d1e4c55b1dd9ccd6d2c0cd0c7c2e2c8
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-12-03 22:42:23 +0700

    update api url

--- lib/services/url_service.dart
+++ lib/services/url_service.dart
@@ -1,5 +1,6 @@
 class UrlService {
-  String baseUrl = 'http://10.0.2.2:8000/';
+  String baseUrl =
+      'https://kostiaraamarta.ashyhill-b518ef35.australiaeast.azurecontainerapps.io/';
   Uri api(String param) {
     return Uri.parse(baseUrl + param);
   }
